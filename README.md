6dd6a676f7fda0dd652c2a08fa8c318d5de55906
jahrei <asulkaitaasan@protonmail.com>
2022-08-31 19:26:25 -0500

    Update files
    Updated README with objective and concept. Initialized main.py.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # selenium_testing
  Messing around with Selenium using Python.
+
+ # The objective
+ Locate text and interact with websites without any user input.
+
+ Upon launching this program, it should be able to grab information from already-opened websites and act upon it.
---
+++ main.py
@@ -0,0 +1,11 @@
+# Import stuff
+import selenium
+
+# Initialize webdriver 
+
+
+# Upon launch of script, grab elements from currently open window 
+# Reference https://stackoverflow.com/questions/8344776/can-selenium-interact-with-an-existing-browser-session
+# Currently unknown if Selenium can interface with already existing browser session
+
+
